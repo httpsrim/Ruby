4287f1138d3cf1666e2bf1fb0289d4bdfbdcb111
Rim <92591903+httpsrim@users.noreply.github.com>
2022-09-13 23:25:31 +0200

    Update String.rb

--- String/String.rb
+++ String/String.rb
@@ -10,3 +10,17 @@ puts cadena.length
 # Utilizacion de `
 
 puts `dir`
+
+# Utilización de <<
+
+cadena = "hello"
+cadena << " World"
+puts cadena
+
+# Utilización END_STR
+
+a = <<END_STR
+  Texto random
+  AAAAA
+END_STR
+puts a
