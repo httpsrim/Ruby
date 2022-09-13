d426ccdda7eee707b912234cc310e3b292a68e85
Rim <92591903+httpsrim@users.noreply.github.com>
2022-09-13 23:10:01 +0200

    Add files via upload

---
+++ String/String.rb
@@ -0,0 +1,12 @@
+# Utilización de #{} (Interpolación)
+
+puts "10 * 134 = #{10 *134}"
+
+# Utilizacion stirng length
+
+cadena = "Cadena de texto"
+puts cadena.length
+
+# Utilizacion de `
+
+puts `dir`
