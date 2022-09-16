b86f5539cb1354f4af4b316c7e565403a6be0b1c
Rim <92591903+httpsrim@users.noreply.github.com>
2022-09-16 15:09:01 +0200

    Add files via upload

---
+++ Comparar/comparar.rb
@@ -0,0 +1,18 @@
+s1= 'hola'
+s2= 'hola'
+
+#forma 1
+if s1.equal?(s2)
+  puts 'Son iguales'
+end
+
+#forma 2
+if s1.eql?(s2)
+  puts 'son iguales'
+end
+
+#forma 3
+
+if(s1 == s2)
+  puts 'son iguales'
+end
